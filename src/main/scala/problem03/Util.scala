73053f646e4b2c4d7ea6403573aa8d5273160e3a
Bohdan Polishchuk <bohdan.polishchuk@nixsolutions.com>
2021-12-12 11:11:28 +0200

    add solution from web

--- src/main/scala/problem03/Util.scala
+++ src/main/scala/problem03/Util.scala
@@ -2,13 +2,19 @@ package problem03
 
 object Util {
   def kthElementTake[A](k: Int, list: List[A]): A =
-    if (list.size > k) list.take(k+1).last else throw new NoSuchElementException
+    if (list.size > k) list.take(k + 1).last else throw new NoSuchElementException
+
+  def kthElementSolutionFromWeb[A](k: Int, list: List[A]): A = (k, list) match {
+    case (0, h :: _) => h
+    case (k, _ :: tail) if (k > 0) => kthElementSolutionFromWeb(k - 1, tail)
+    case _ => throw new NoSuchElementException
+  }
 
   def kthElementRecursive[A](k: Int, list: List[A]): A = {
     k match {
-      case _ if(k>= list.size && list == Nil) => throw new NoSuchElementException
+      case _ if (k >= list.size && list == Nil) => throw new NoSuchElementException
       case 0 => list.head
-      case _ => kthElementRecursive(k-1,list.tail)
+      case _ => kthElementRecursive(k - 1, list.tail)
     }
   }
 }
